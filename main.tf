681954789486d2931b3db5ad3ef2dec36b2e3bdf
Liem Nguyen <nguyliem@sheridancollege.ca>
2022-04-06 13:56:05 -0400

    This is my commit for Part3 VPC

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = us-east-1
-}
-
 resource "aws_vpc" "liempart3" {
   cidr_block           = "10.0.0.0/16"
   tags                 = "Terraform VPC for Part3"
