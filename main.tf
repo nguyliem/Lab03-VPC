5a2124bfa24d99b13db144100de53f8072e3696e
Liem Nguyen <nguyliem@sheridancollege.ca>
2022-04-06 14:01:25 -0400

    This is my commit for Part3 VPC

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc" "liempart3" {
-  cidr_block           = "10.0.0.0/16"
-  tags                 = "Terraform VPC for Part3"
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Liem Part3"
+  }
 }
